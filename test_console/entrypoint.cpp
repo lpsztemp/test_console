1258d58df650c3ff9944dc46844243db982a93a7
Andrei Chusov <lpsztemp@gmail.com>
2017-04-09 16:43:34 +1000

    Refactoring of the server

--- test_console/entrypoint.cpp
+++ test_console/entrypoint.cpp
@@ -8,23 +8,23 @@ using namespace CAMaaS;
 
 std::string operator""_s(const char* psz, std::size_t cch) {return std::string(psz, cch);}
 
-void TestContiguousDataSource(OutputBinaryDataContiguousAccessRef output_side, InputBinaryDataContiguousAccessRef input_side)
+void TestContiguousDataSource(ContiguousDataStorageOutputRef output_side, ContiguousDataStorageInputRef input_side)
 {
-	auto osWrite = output_side.write();
+	auto osWrite = represent_as<OutputByteStreamOwn>(output_side.write());
 	osWrite.write(std::vector<std::uint8_t>({0x30, 0x31, 0x32, 0x33, 0x34}));
 	osWrite.write(std::vector<std::uint8_t>({0x35, 0x36, 0x37, 0x38, 0x39}));
-	auto osWrite2 = output_side.write(20);
+	auto osWrite2 = represent_as<OutputByteStreamOwn>(output_side.write(20));
 	osWrite2.write(std::vector<std::uint8_t>({'a', 'b', 'c', 'd', 'e'}));
 	osWrite2.write(std::vector<std::uint8_t>({'e', 'f', 'g', 'h', 'i'}));
-	output_side.delete_block(10, 10);
-	auto osRead1 = input_side.read();
+	represent_as<ContiguousDataStorageOwn>(output_side).erase(10, 10);
+	auto osRead1 = represent_as<InputByteStreamOwn>(input_side.read());
 	auto v1 = osRead1.read_as<std::vector<std::uint8_t>>(10);
 	auto vout10 = osRead1.read_as<std::vector<std::uint8_t>>(5);
-	auto osRead2 = input_side.read(15);
-	auto vout11 = osRead2.read_as<std::vector<std::uint8_t>>(std::size_t(input_side.size() - 15));
+	auto osRead2 = represent_as<InputByteStreamOwn>(input_side.read(15));
+	auto vout11 = osRead2.read_as<std::vector<std::uint8_t>>(std::size_t(input_side.byte_size() - 15));
 	std::move(vout10.begin(), vout10.end(), std::back_inserter(v1));
 	std::move(vout11.begin(), vout11.end(), std::back_inserter(v1));
-	auto v2 = input_side.read().read_all_as<std::vector<std::uint8_t>>();
+	auto v2 = represent_as<InputByteStreamOwn>(input_side.read()).read_all_as<std::vector<std::uint8_t>>();
 	std::cout << "Vectors are equal: " << std::boolalpha << (v1 == v2) << "\n";
 	std::cout << "Read contents: ";
 	for (auto b:v2)
@@ -53,10 +53,10 @@ bool copy(IInputByteStream* pFrom, IOutputByteStream* pTo)
    while(cbRead != 0)
    {
       cbRead = WINDOW_SIZE;
-      std::int32_t nError = pFrom->Read(buf, std::addressof(cbRead));
+      std::int32_t nError = pFrom->ReadArray(buf, std::addressof(cbRead));
       if (nError < 0)
          return false; //error
-      nError = pTo->Write(buf, cbRead);
+      nError = pTo->WriteArray(buf, cbRead);
       if (nError < 0)
          return false; //error
    }
@@ -66,7 +66,7 @@ bool copy(IInputByteStream* pFrom, IOutputByteStream* pTo)
 template <class...Args>
 static auto deduce_iterator_range(Args&&...args) -> 
 	typename std::enable_if<
-		_Implementation::is_byte_type<
+		is_byte_type<
 			typename std::iterator_traits<
 				typename Chusov::identity<
 					decltype(_Implementation::deduce_iterator_range(std::forward<Args>(args)...))
@@ -88,39 +88,25 @@ struct is_deducable_to_range:decltype(_is_deducable_to_range<Args...>(int())) {}
 
 int main(int, char**)
 {
-	_Implementation::is_byte_type<void>::value;
-	static_assert(is_deducable_to_range<void*, std::size_t>::value, "");
-	auto rng = _Implementation::deduce_iterator_range(nullptr, std::size_t());
-	static_assert(is_deducable_to_range<std::uint8_t*, std::size_t>::value, "");
-	static_assert(!is_deducable_to_range<_Implementation::MapBasedKeyedDataBaseImplementation::key_type>::value, "");
+	auto map = CKeyedData<_Implementation::InMemoryAssociativeDataStorageImplementation>();
 
-	auto p = _Implementation::KeyedDataBaseBridge<_Implementation::MapBasedKeyedDataBaseImplementation>();
-	////auto fail = CKeyedData<_Implementation::MapBasedKeyedDataBaseImplementation>();
-	//auto b1 = _Implementation::KeyedDataBaseBridge<_Implementation::MapBasedKeyedDataBaseImplementation>::is_readable_type::value;
-	//auto b2 = _Implementation::KeyedDataBaseBridge<_Implementation::MapBasedKeyedDataBaseImplementation>::is_writable_type::value;
+	map.insert("Key1", 4, std::string("value"));
+	map.insert(reinterpret_cast<const std::uint8_t*>("Key2"), 4, "value", 5);
+	map.insert(std::string("Key3"), "value", 5);
+	map.insert(static_cast<const void*>("Key4"), 4, static_cast<const void*>("value"), 6);
+	//std::string strKey = "Key5";
+	//map.insert(strKey, make_memory_buffer_input_stream("value").get());
+	//strKey = "Key6";
+	//map.insert(strKey.begin(), strKey.end(), 
+	//	ConsequentDataStorageInputOwn(make_binary_memory_storage_adapter(std::string("value")).get()).read().get_interface());
+	auto v = map.find("Key3", 3);
+	map.erase(v);
 
-
-	p.Insert(std::piecewise_construct, std::forward_as_tuple("Key", 3), std::forward_as_tuple(make_binary_memory_storage_adapter(std::string("value")).get()));
-	p.Insert(std::piecewise_construct, std::forward_as_tuple(reinterpret_cast<const std::uint8_t*>("Key"), 3), std::forward_as_tuple(make_binary_memory_storage_adapter(std::string("value")).get()));
-	p.Insert(std::piecewise_construct, std::forward_as_tuple(static_cast<const void*>("Key"), 3), std::forward_as_tuple(make_binary_memory_storage_adapter(std::string("value")).get()));
-	std::string strKey = "Some";
-	p.Insert(strKey, make_binary_memory_storage_adapter(std::string("value")).get());
-	p.Insert(std::piecewise_construct, std::forward_as_tuple(strKey), std::forward_as_tuple(make_binary_memory_storage_adapter(std::string("value")).get()));
-	p.Insert(std::piecewise_construct, std::forward_as_tuple(strKey.begin(), strKey.end()), std::forward_as_tuple(make_binary_memory_storage_adapter(std::string("value")).get()));
-	p.Insert(std::piecewise_construct, std::forward_as_tuple(std::string("Key 1")), std::forward_as_tuple(make_binary_memory_storage_adapter(std::string("value")).get()));
-	p.Insert(std::string("Key 2"), make_binary_memory_storage_adapter(std::string("value")).get());
-	p.Insert(_Implementation::MapBasedKeyedDataBaseImplementation::key_type("Key 3", 5),
-		_Implementation::MapBasedKeyedDataBaseImplementation::value_type(make_binary_memory_storage_adapter(std::string("value")).get()));
-	p.Insert(std::piecewise_construct, std::forward_as_tuple(_Implementation::MapBasedKeyedDataBaseImplementation::key_type("Key 4", 5)),
-		std::forward_as_tuple(_Implementation::MapBasedKeyedDataBaseImplementation::value_type(make_binary_memory_storage_adapter(std::string("value")).get())));
-	auto v = p.Find("Key", 3);
-	p.Erase(v);
-
-
-	//auto mp = _Implementation::MapBasedKeyedDataBaseImplementation();
-	//mp.insert(std::piecewise_construct, std::forward_as_tuple("Key", 3), std::forward_as_tuple(make_binary_memory_storage_adapter(std::string("value")).get()));
-
-	/*std::cout << "===File test===\n";
+	//auto strFile = u8"Test file.txt"_s;
+	//auto file = represent_as<ContiguousDataStorageOwn>(make_file_based_data_storage<FileReadWrite, FileCreateAlways>(strFile));
+	//ConsequentDataStorageInputRef ref = file;
+	
+	std::cout << "===File test===\n";
 	auto strFile = u8"Test file.txt"_s;
 	auto file = make_file_based_data_storage<FileReadWrite, FileCreateAlways>(strFile);
 	TestContiguousDataSource(file, file);
@@ -134,23 +120,23 @@ int main(int, char**)
 	auto buff2 = make_inmemory_preallocated_data_storage(own_buffer(p2, sizeof(pBuffer), alloc));
 	TestContiguousDataSource(buff2, buff2);
 	std::cout << "===Reading from existing buffer===\n";
-	auto pRead = make_inmemory_input_data_source(pBuffer, buff.size());
-	auto vRead = pRead.read().read_all_as<std::vector<std::uint8_t>>();
+	auto pRead = make_inmemory_input_data_source(pBuffer, buff.byte_size());
+	auto vRead = represent_as<InputByteStreamOwn>(pRead.read()).read_all_as<std::vector<std::uint8_t>>();
 	std::cout << "Read contents from the input buffer: ";
 	for (auto b:vRead)
 		std::cout << std::hex << b;
 	std::cout << "\n";
 	try
 	{
-		represent_as<OutputBinaryDataOwn>(pRead);
-		std::cout << "Conversion did NOT cause an expected error!\n";
+		represent_as<ConsequentDataStorageOwn>(pRead);
+		std::cout << "Conversion did NOT cause an expected error\n";
 	}catch (Chusov::Exceptions::ChsvCodeException& ex)
 	{
 		std::cout << "Conversion caused expected error: " << ex.what() << "\n";
 	}
 	std::cout << "===Cached buffer test===\n";
 	auto buff_cached = make_inmemory_data_storage();
-	TestContiguousDataSource(buff_cached, buff_cached);*/
+	TestContiguousDataSource(buff_cached, buff_cached);
 
 	return 0;
 	
